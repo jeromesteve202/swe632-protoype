48e89655c99acc5a61a693f008cbdbefdd16b0b9
Steve Sahayadarlin <jeromesteve202@gmail.com>
2024-02-05 10:27:11 -0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { AiOutlineDelete } from "react-icons/ai";
+import { FcCheckmark } from "react-icons/fc";
 
 function App() {
+
+  const [isCompleteScreen, setCompleteScreen] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Assignment Tracker
+
+        <div className='assignment-wrapper'>
+          <div className='assignment-input'>
+            <div className='assignment-input-item'>
+              <label>Title</label>
+              <input type="text" placeholder="What's the assignment name?" />
+            </div>
+            <div className='assignment-input-item'>
+              <label>Description</label>
+              <input type="text" placeholder="What's the assignment description?" />
+            </div>
+            <div className='assignment-input-item'>
+              <button type='button' className='primary-button'>Add</button>
+            </div>
+          </div>
+
+          <div className='button-area'>
+            <button className={`secondary-button ${isCompleteScreen===false && 'active'}`} onClick={() => setCompleteScreen(false)}>In Progress</button>
+            <button className={`secondary-button ${isCompleteScreen===true && 'active'}`} onClick={() => setCompleteScreen(true)}>Completed</button>
+          </div>
+
+          <div className='assignment-list'>
+            <div className='assignment-list-item'>
+              <div>
+                <h3>Task 1</h3>
+                <p>Description</p>
+              </div>
+
+              <div>
+                <AiOutlineDelete className='icon'/>
+                <FcCheckmark className='check-icon'/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </h1>
     </div>
   );
 }
